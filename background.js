fc9ba8d0c15f9b80a924bc13b07dd793f866c901
AleksandrTheGreat21 <100033042@mvla.net>
2025-02-14 10:15:15 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 let isRunning = true;
 async function openTabs() {
     while (isRunning) {
-        chrome.tabs.create({ url: "https://www.google.com" });
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
         await new Promise(resolve => setTimeout(resolve, 1000)); // Opens every 2 seconds
     }
 }
