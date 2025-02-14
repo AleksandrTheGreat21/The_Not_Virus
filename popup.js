fe72073a21d0b7305251a5ecfab2f755985be4c5
Hedgehawk11 <84346970+Hedgehawk11@users.noreply.github.com>
2025-02-14 09:45:44 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
+// JavaScript to open the Rick Roll video
+function openRickRoll() {
+  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+}
+
+//John wait, does'nt JS not work in uncertified Chrome extensions?
+
 document.getElementById("start").addEventListener("click", () => {
     chrome.runtime.sendMessage({ action: "startOpeningTabs" });
 });
